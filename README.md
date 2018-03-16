dec571b283a4b61bc9b3247dd18b72a947116d95
shawn rynearson <shawn.rynearson@gmail.com>
2018-03-15 21:23:17 -0600

    test upload and README

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# vmccl
-Command line lightweight implementation of the VMC SHA-512 algorithm
+## Variation Modelling Collaboration Command Line tool.
+
+Lightweight command line implementation of the VMC SHA-512 algorithm to allow rapid digest generation and comparison from fasta files, STDIN or text blob.
+
+**Please keep in mind this tool only implements the VMC Sequence digest aspect of the entire [VMC model](https://docs.google.com/document/d/12E8WbQlvfZWk5NrxwLytmympPby6vsv60RxCeD5wc1E/edit). No JSON files are created or validated.**
+
+```
+$> vmccl -h
+Usage: vmccl [--stdin] [--blob BLOB] [--vmc] [--fasta FASTA] [--length LENGTH]
+
+Options:
+  --stdin                Read from stdin.
+  --blob BLOB            Blob text to hash using the SHA-512 algorithm.
+  --vmc                  With output the result of the above blob/stdin base on the current VMC spec.
+  --fasta FASTA          Will return VMC digest version of this fasta file.
+  --length LENGTH        Length of digest id to return. [default: 24]
+  --help, -h             display this help and exit
+```
+
+Adding the `--vmc` option will return a base64 URL Encoded ID. All other options 
+are self-explanatory with examples shown below.
+
+## Installing
+
+Easist method to run `vmccl` is to download the executable corresponding to your computer environment.
+
+*  Download
+
+OS | Platform | Link
+---|---|---
+darwin | amd64 
+linux | amd64 | 
+
+* Then run
+
+```
+$> vmccl_linux64 vmccl
+```
+
+
+## Examples:
+
+All `--stdin` and `blob` text will have newlines and spaces removed.
+
+#### stdin option:
+
+This example of stdin show the output with and without the `--vmc` option added 
+
+```
+$> wc -lmw irobot.txt
+6041   70216  403176 irobot.txt
+
+$> cat irobot.txt | vmccl --stdin
+mbeo1K0MZwIHizAurCs2hYwA7LMyXSX0
+
+$> cat irobot.txt | vmccl --stdin --vmc
+mbeo1K0MZwIHizAurCs2hYwA7LMyXSX0
+
+$> cat irobot.txt | vmccl --stdin --length 60
+mbeo1K0MZwIHizAurCs2hYwA7LMyXSX0OQtVedGfpGBChEOV4jv58F1SeXpq0K5rUGsytqHm4/1oicIh
+
+$> cat irobot.txt | vmccl --stdin --vmc --length 60
+mbeo1K0MZwIHizAurCs2hYwA7LMyXSX0OQtVedGfpGBChEOV4jv58F1SeXpq0K5rUGsytqHm4_1oicIh
+
+```
+
+#### blob option:
+
+```
+$> vmccl --blob "I, Robot Isaac Asimov. TO JOHN W. CAMPBELL, JR, who godfathered THE ROBOTS"
+p6WvpVcb0/hJj5Y/4Za3o01Ln40R+Ijz
+
+$> vmccl --blob "I, Robot Isaac Asimov. TO JOHN W. CAMPBELL, JR, who godfathered THE ROBOTS" --vmc
+p6WvpVcb0_hJj5Y_4Za3o01Ln40R-Ijz
+
+``` 
+
+#### fasta option:
+
+```
+$> vmccl.go --fasta NC_000019.10.fasta
+
+Description line:  NC_000019.10 Homo sapiens chromosome 19, GRCh38.p7 Primary Assembly
+VMCDigest ID:  IIB53T8CNeJJdUqzn9V_JnRtQadwWCbl
+```
+
---
+++ vmccl.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha512"
+	"encoding/base64"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/alexflint/go-arg"
+	"github.com/shenwei356/bio/seqio/fastx"
+)
+
+func main() {
+
+	var args struct {
+		Stdin  bool   `help:"Read from stdin."`
+		Blob   string `help:"Blob text to hash using the SHA-512 algorithm."`
+		VMC    bool   `help:"With output the result of the above blob/stdin base on the current VMC spec."`
+		Fasta  string `help:"Will return VMC Sequence digest of this fasta file."`
+		Length int    `help:"Length of digest id to return."`
+	}
+	args.Length = 24
+	arg.MustParse(&args)
+
+	if len(args.Fasta) > 1 {
+		digestFasta(args.Fasta, args.Length)
+	} else if len(args.Blob) > 1 {
+
+		clean := scrubber(args.Blob)
+		byteForm := []byte(clean)
+
+		if args.VMC {
+			fmt.Println(VMCDigest(byteForm, args.Length))
+		} else {
+			fmt.Println(Digest(byteForm, args.Length))
+		}
+
+	} else if args.Stdin {
+		scanner := bufio.NewScanner(os.Stdin)
+
+		stdinText := ""
+		for scanner.Scan() {
+			stdinText += scanner.Text()
+		}
+		clean := scrubber(stdinText)
+		toByte := []byte(clean)
+
+		if args.VMC {
+			fmt.Println(VMCDigest(toByte, args.Length))
+		} else {
+			fmt.Println(Digest(toByte, args.Length))
+		}
+	}
+}
+
+// --------------------------------------------------------------------- //
+
+func scrubber(i string) string {
+	cleanEnds := strings.TrimSpace(i)
+	noSpace := strings.Replace(cleanEnds, " ", "", -1)
+
+	return noSpace
+}
+
+// --------------------------------------------------------------------- //
+
+func digestFasta(file string, length int) {
+
+	// Lifted from gofasta-vmc.go
+	// Incoming fastq file.
+	reader, err := fastx.NewDefaultReader(file)
+	if err != nil {
+		panic(err)
+	}
+
+	for chunk := range reader.ChunkChan(5000, 5) {
+		if chunk.Err != nil {
+			panic(chunk.Err)
+		}
+
+		for _, record := range chunk.Data {
+			digestID := VMCDigest(record.Seq.Seq, length)
+
+			fmt.Println("Description line: ", string(record.Name))
+			fmt.Println("VMCDigest ID: ", digestID)
+		}
+	}
+}
+
+// --------------------------------------------------------------------- //
+
+// Non VMC URLEncoding hash.
+func Digest(bv []byte, truncate int) string {
+	hasher := sha512.New()
+	hasher.Write(bv)
+
+	sha := base64.StdEncoding.EncodeToString(hasher.Sum(nil)[:truncate])
+	return sha
+}
+
+// --------------------------------------------------------------------- //
+
+// VMC implemented digest
+func VMCDigest(bv []byte, truncate int) string {
+	hasher := sha512.New()
+	hasher.Write(bv)
+
+	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil)[:truncate])
+	return sha
+}
+
+// --------------------------------------------------------------------- //
